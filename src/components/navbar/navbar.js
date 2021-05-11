aa55d962415ff2c214baccc023c6896fdcd11ee9
ckvaizz <vaisakh.k591@gmail.com>
2021-05-11 22:51:33 +0530

    nav-added

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,7 +5,11 @@ const Navbar = () => {
     return(
         <div className="outer-nav">
             <div className="inner-nav">
-                
+                <h1>Covid-Dashboard</h1>
+                <h3></h3>
+                <h3>Home</h3>
+                <h3>Hospitals</h3>
+                <h3>Ambulance</h3>
             </div>
         </div>
     )
