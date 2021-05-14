e6df9279aba202f700248d5ed27712741694f0aa
ckvaizz <vaisakh.k591@gmail.com>
2021-05-14 19:49:41 +0530

    api work-completed

--- src/components/ambulance/ambulance.js
+++ src/components/ambulance/ambulance.js
@@ -1,78 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './ambulance.css';
 import emergencyimg from '../../images/emergency5.jpg';
+import Axios from 'axios';
 
 
 const Ambulance = () => {
+    const [apiData,setApiData] = useState([])
+    const [allStates,setAllStates] = useState([])
+    const [selectedState,setSelectedState] =useState('')
+
+    useEffect( ()=>{
+        Axios.get('https://life-api.coronasafe.network/data/ambulance_verified.json').then(async resp=>{
+            setApiData(resp.data.data);
+           
+                await Promise.all(resp.data.data.map(async(d,k)=>{
+                   // console.log(typeof(allStates[0]))
+                   
+                   setAllStates(arr=>[...arr,d.state])
+                //    else console.log(existing)
+                
+                
+                    
+        }) )
+       // setAllStates([...new Set(allStates)])
+      
+             
+    })
+             // setAllstate(Object.keys(allStateOBj))
+      
+    },[])
+    const stateChangeHandler = e=>{
+        if(e.target.value === "") return  setSelectedState('')
+        setSelectedState(e.target.value)
+    }
     return(
         <div className="amb-main">
             <h2>Ambulance</h2>
-            <select>
-            <option value="0">State</option>
-                    <option value="1">kerala</option>
-                    <option value="2">Tamilnadu</option>
-                    <option value="3">Karnataka</option>
-                    <option value="4">Goa</option>
-                    <option value="1">kerala</option>
-                    <option value="2">Tamilnadu</option>
-                    <option value="3">Karnataka</option>
-                    <option value="4">Goa</option>
+            <select onChange={stateChangeHandler}> 
+            <option  value="">State</option>
+            {
+                allStates.length !==0?
+                
+                allStates.map((e,k)=>{
+                    return<option key={k} value={e}>{e}</option>
+                }):''
+            }
+                    
+                   
             </select>
             <div className="img-one">
             <img src={emergencyimg} alt="ll"/>
             </div>
         <div className="amb-card-main">
-            <div className="amb-card">
-            <div className="amb-sec1">
-                <h3>District</h3>
-                <h4>Area</h4>
-                <h4>Phone</h4>
-            </div>
-            
-            <h4>Lorem ipsum, or lipsum as it is sometimes known,
-                is dummy text used in laying out print
-            </h4>
-            
-            </div>
+        {
+                    selectedState && apiData?
+                    apiData.map(d=>{
+                        return(
+                            d.state === selectedState ?
+                          <div className="amb-card">
+                            <div className="amb-sec1">
+                                <h3>{d.district}</h3>
+                                <h4>{d.area!==null?d.area:''}</h4>
+                                <h4>{d.phone1===null?d.phone2:d.phone1}</h4>
+                            </div>
+                            
+                            <h4>{d.comment}
+                            </h4>
+                            
+                            </div>
 
-            <div className="amb-card">
-            <div className="amb-sec1">
-                <h3>District</h3>
-                <h4>Area</h4>
-                <h4>Phone</h4>
-            </div>
-            
-            <h4>Lorem ipsum, or lipsum as it is sometimes known,
-                is dummy text used in laying out print
-            </h4>
-            
-            </div>
+                            : console.log('no')
+                        
+                            )
+                                
+                            
+                            
+                        }):''
+                    }
 
-            <div className="amb-card">
-            <div className="amb-sec1">
-                <h3>District</h3>
-                <h4>Area</h4>
-                <h4>Phone</h4>
-            </div>
             
-            <h4>Lorem ipsum, or lipsum as it is sometimes known,
-                is dummy text used in laying out print
-            </h4>
-            
-            </div>
 
-            <div className="amb-card">
-            <div className="amb-sec1">
-                <h3>District</h3>
-                <h4>Area</h4>
-                <h4>Phone</h4>
-            </div>
-            
-            <h4>Lorem ipsum, or lipsum as it is sometimes known,
-                is dummy text used in laying out print
-            </h4>
-            
-            </div>
+          
 
         </div>
 
--- src/components/medicine/medicine.js
+++ src/components/medicine/medicine.js
@@ -1,86 +1,83 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './medicine.css';
-
+import Axios from 'axios'
 
 const Medicine = () =>{ 
+    const [apiData,setApiData] = useState([])
+    const [allStates,setAllStates] = useState([])
+    const [selectedState,setSelectedState] =useState('')
+
+    useEffect( ()=>{
+        Axios.get('https://life-api.coronasafe.network/data/medicine_verified.json').then(async resp=>{
+            setApiData(resp.data.data);
+           
+                await Promise.all(resp.data.data.map(async(d,k)=>{
+                   // console.log(typeof(allStates[0]))
+                   
+                   setAllStates(arr=>[...arr,d.state])
+                //    else console.log(existing)
+                
+                
+                    
+        }) )
+       // setAllStates([...new Set(allStates)])
+      
+             
+    })
+             // setAllstate(Object.keys(allStateOBj))
+      
+    },[])
+
+    const stateChangeHandler = e=>{
+        if(e.target.value === "") return  setSelectedState('')
+        setSelectedState(e.target.value)
+    }
     return(
         <div className="med-main">
             <h2>Medicine</h2>
-            <select>
-            <option value="0">State</option>
-                    <option value="1">kerala</option>
-                    <option value="2">Tamilnadu</option>
-                    <option value="3">Karnataka</option>
-                    <option value="4">Goa</option>
-                    <option value="1">kerala</option>
-                    <option value="2">Tamilnadu</option>
-                    <option value="3">Karnataka</option>
-                    <option value="4">Goa</option>
+            <select onChange={stateChangeHandler}> 
+            <option  value="">State</option>
+            {
+                allStates.length !==0?
+                
+                allStates.map((e,k)=>{
+                    return<option key={k} value={e}>{e}</option>
+                }):''
+            }
+                    
+                   
             </select>
             <div className="medi-card-main">
-                <div className="medi-card1">
+            {
+                    selectedState && apiData?
+                    apiData.map(d=>{
+                        return(
+                            d.state === selectedState ?
+                            <div className="medi-card1">
+                
                     <div className="top-curve1">
-                        <h2>Name</h2>
-                        <h5>Lorem ipsum, or lipsum as it is sometimes known,
-                             is dummy text used in laying out print, graphic or web designs.
-                             The passage is attributed to an unknown 
+                        <h2>{d.name}</h2>
+                        <h5>{d.address}
                             </h5>
 
                     </div>
-                    <h4>Address</h4>
-                    <h4>District</h4>
-                    <h4>City</h4>
-                    <h4>Email</h4>
-                    <h4>Phone</h4>
+                    <h4>{d.comment}</h4>
+                    <h4>{d.district}</h4>
+                    
+                    <h4>{d.emailId}</h4>
+                    <h4>{d.phone1}</h4>
                 </div>
+                            : console.log('no')
+                        
+                            )
+                                
+                            
+                            
+                        }):''
+                    }
+                
 
-                <div className="medi-card1">
-                    <div className="top-curve1">
-                        <h2>Name</h2>
-                        <h5>Lorem ipsum, or lipsum as it is sometimes known,
-                             is dummy text used in laying out print, graphic or web designs.
-                             The passage is attributed to an unknown 
-                            </h5>
-
-                    </div>
-                    <h4>Address</h4>
-                    <h4>District</h4>
-                    <h4>City</h4>
-                    <h4>Email</h4>
-                    <h4>Phone</h4>
-                </div>
-
-                <div className="medi-card1">
-                    <div className="top-curve1">
-                        <h2>Name</h2>
-                        <h5>Lorem ipsum, or lipsum as it is sometimes known,
-                             is dummy text used in laying out print, graphic or web designs.
-                             The passage is attributed to an unknown 
-                            </h5>
-
-                    </div>
-                    <h4>Address</h4>
-                    <h4>District</h4>
-                    <h4>City</h4>
-                    <h4>Email</h4>
-                    <h4>Phone</h4>
-                </div>
-
-                <div className="medi-card1">
-                    <div className="top-curve1">
-                        <h2>Name</h2>
-                        <h5>Lorem ipsum, or lipsum as it is sometimes known,
-                             is dummy text used in laying out print, graphic or web designs.
-                             The passage is attributed to an unknown 
-                            </h5>
-
-                    </div>
-                    <h4>Address</h4>
-                    <h4>District</h4>
-                    <h4>City</h4>
-                    <h4>Email</h4>
-                    <h4>Phone</h4>
-                </div>
+               
             </div>
 
                 
